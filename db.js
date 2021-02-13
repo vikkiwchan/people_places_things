c67ff6a26ee87937c955f6e6c2ebcaccdced92d0
Dominique Sobieski <sobieski.d@gmail.com>
2021-02-13 12:16:15 -0500

    try 2

--- db.js
+++ db.js
@@ -18,16 +18,47 @@ const Place = db.define('place', {
   },
 });
 
-const Date = db.define('date', {
-  date: {
-    type: DATE,
-  },
-  validate: {
-    isDate: true,
-  },
-});
+const Thing = db.define('thing', {
+  name: {
+    type: STRING
+  }
+})
+
+// const Date = db.define('date', {
+//   date: {
+//     type: DATE,
+//   },
+//   validate: {
+//     isDate: true,
+//   },
+// });
+
+// est associations
+
+Thing.belongsTo(People);
+People.hasMany(Thing);
+Place.belongsTo(People);
+People.hasMany(Place);
 
 const syncAndSeed = async () => {
   await db.sync({ force: true });
+  const [moe, lucy, larry] = await Promise.all(
+    ['moe', 'lucy', 'larry'].map(name => People.create({name})
+  ));
+  const [NYC, Chicago, LA, Dallas] = await Promise.all(
+    ['NYC', 'Chicago', 'LA', 'Dallas'].map(name => Place.create({name})
+  ));
+  const [foo, bar, bazz, quq] = await Promise.all(
+    ['foo', 'bar', 'bazz', 'quq'].map(name => Thing.create({name})
+  ));
   console.log('----DB CONNECTED!----');
 };
+
+module.exports = {
+  syncAndSeed,
+  models: {
+      People, 
+      Place,
+      Thing
+  }
+}
--- server.js
+++ server.js
@@ -1,13 +1,52 @@
-const { syncAndSeed } = require('./db');
+const { syncAndSeed, models: {People, Place, Thing} } = require('./db');
+const express = require('express');
+//const html = require('html-template-tag');
+const app = express()
+const error = 'ahh'
+app.get('/', async(req, res, next) => {
+  try {
+    const [people, places, things] = await Promise.all([
+      People.findAll(),
+      Place.findAll(),
+      Thing.findAll()
+    ])
+    res.send(
+      `<html>
+        <head>
+          <title>People, Places, Things</title>
+        </head>
+        <body>
+          <h1>People, Places, Things</h1>
+          <div>
+            <h2>People</h2>
+            <ul>
+              ${people.map(
+                (person) => `
+                  <li>
+                    ${person.name}
+                  </li>
+                `
+              ).join('')}
+            </ul>
+          </div>
+          </body>
+          </html>`
+    )
+  }
+  catch (error) {
+    console.error(error)
+  }
+})
+
 
 const init = async () => {
   try {
     await syncAndSeed();
     const port = process.env.PORT || 3000;
-    app.lister(port, () => console.log(`listening on port ${port}`));
+    app.listen(port, () => console.log(`listening on port ${port}`));
     console.log('ready');
   } catch (error) {
-    next(error);
+    console.error(error)
   }
 };
 
