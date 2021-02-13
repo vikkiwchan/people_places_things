b25c6a8cc75f4ebb7418e8e14d45410b88f7af1d
Vikki Chan <vikkiwchan@gmail.com>
2021-02-13 11:42:13 -0600

    css started

--- server.js
+++ server.js
@@ -1,43 +1,61 @@
-const { syncAndSeed, models: {People, Place, Thing} } = require('./db');
+const {
+  syncAndSeed,
+  models: { People, Place, Thing },
+} = require('./db');
 const express = require('express');
-//const html = require('html-template-tag');
-const app = express()
-const error = 'ahh'
-app.get('/', async(req, res, next) => {
+const html = require('html-template-tag');
+const path = require('path');
+
+const app = express();
+const error = 'ahh';
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', async (req, res, next) => {
   try {
     const [people, places, things] = await Promise.all([
       People.findAll(),
       Place.findAll(),
-      Thing.findAll()
-    ])
+      Thing.findAll(),
+    ]);
     res.send(
-      `<html>
+      html`<html>
         <head>
           <title>People, Places, Things</title>
+          <link rel="stylesheet" href="/public/styles.css" />
         </head>
         <body>
           <h1>People, Places, Things</h1>
-          <div>
-            <h2>People</h2>
-            <ul>
-              ${people.map(
-                (person) => `
-                  <li>
-                    ${person.name}
-                  </li>
-                `
-              ).join('')}
-            </ul>
+          <div id="container">
+            <div id="list">
+              <div>
+                <h2>People</h2>
+                <ul>
+                  ${people.map((person) => html` <li>${person.name}</li> `)}
+                </ul>
+              </div>
+              <div>
+                <h2>Places</h2>
+                <ul>
+                  ${places.map((place) => html` <li>${place.name}</li> `)}
+                </ul>
+              </div>
+              <div>
+                <h2>Things</h2>
+                <ul>
+                  ${things.map((thing) => html` <li>${thing.name}</li> `)}
+                </ul>
+              </div>
+            </div>
+            <div id="form">grggughddid</div>
           </div>
-          </body>
-          </html>`
-    )
+        </body>
+      </html>`
+    );
+  } catch (error) {
+    console.error(error);
   }
-  catch (error) {
-    console.error(error)
-  }
-})
-
+});
 
 const init = async () => {
   try {
@@ -46,7 +64,7 @@ const init = async () => {
     app.listen(port, () => console.log(`listening on port ${port}`));
     console.log('ready');
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 };
 
